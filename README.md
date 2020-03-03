19a1b95a8e359b3d61013e3a9fe76327f5b1ca66
Harman <hrmnjt@hrmn.in>
2020-03-03 23:50:47 +0400

    Adding Golang and cobra scaffolding

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Notes
 
-Coming soon!
+Note down privately!
+
+Commands allowed:
+```console
+Long description for root notes
+
+Usage:
+  notes [flags]
+  notes [command]
+
+Available Commands:
+  help        Help about any command
+  new         A brief description of your command
+  pull        A brief description of your command
+  push        A brief description of your command
+  save        A brief description of your command
+
+Flags:
+      --config string   config file (default is $HOME/.notes.yaml)
+  -h, --help            help for notes
+  -t, --toggle          Help message for toggle
+
+Use "notes [command] --help" for more information about a command.
+```
---
+++ cmd/save.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// saveCmd represents the save command
+var saveCmd = &cobra.Command{
+	Use:   "save",
+	Short: "Short description for saving notes",
+	Long:  `Long description for saving notes`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("save called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(saveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// saveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// saveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
